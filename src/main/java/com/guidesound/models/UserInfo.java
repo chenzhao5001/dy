fccfc085b7bc9aa027b92eab1db584a662d5ffd9
chen <chenzhao5001@163.com>
2018-10-21 15:05:58 +0800

    用户接口

---
+++ src/main/java/com/guidesound/models/UserInfo.java
@@ -0,0 +1,103 @@
+package com.guidesound.models;
+
+public class UserInfo {
+    private int id;
+    private String phone;
+    private String name;
+    private String head;
+    private int type;
+    private int level;
+    private int status;
+    private String sign_name;
+    private int sex;
+    private int teach_age;
+    private int create_time;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getSign_name() {
+        return sign_name;
+    }
+
+    public void setSign_name(String sign_name) {
+        this.sign_name = sign_name;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public int getTeach_age() {
+        return teach_age;
+    }
+
+    public void setTeach_age(int teach_age) {
+        this.teach_age = teach_age;
+    }
+
+    public int getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(int create_time) {
+        this.create_time = create_time;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
