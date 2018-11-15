0d7ea8b80329326457f58d6f1ac48625519d37ac
chen <chenzhao5001@163.com>
2018-11-15 13:41:13 +0800

    验证码相关逻辑

---
+++ src/main/java/com/guidesound/controller/ManagerController.java
@@ -0,0 +1,72 @@
+package com.guidesound.controller;
+
+import com.guidesound.util.JSONResult;
+import com.guidesound.util.VerifyCodeUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/admin")
+public class ManagerController {
+
+    @RequestMapping(value = "/login")
+    @ResponseBody
+    JSONResult logIn(HttpServletRequest request, HttpServletResponse response) {
+
+        String account = request.getParameter("account");
+        String pwd = request.getParameter("pwd");
+        String code = request.getParameter("code");
+        if(account == null || pwd == null || code == null) {
+            JSONResult.errorMsg("缺少参数");
+        }
+        HttpSession session = request.getSession(true);
+        String temp = (String) session.getAttribute("verCode");
+
+        if (temp == null || temp != code) {
+            return JSONResult.errorMsg("验证码错误");
+        }
+
+        
+
+        return null;
+    }
+
+    @RequestMapping(value = "/code")
+    void identifyingCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String verifyCode = VerifyCodeUtils.generateVerifyCode(4);
+        HttpSession session = request.getSession(true);
+        session.removeAttribute("verCode");
+        session.setAttribute("verCode", verifyCode.toLowerCase());
+
+        int w = 100, h = 40;
+        VerifyCodeUtils.outputImage(w, h, response.getOutputStream(), verifyCode);
+    }
+
+
+    @RequestMapping(value = "/current_video_list")
+    @ResponseBody
+    JSONResult currentVideoList(HttpServletRequest request, HttpServletResponse response) {
+        return null;
+    }
+
+
+    @RequestMapping(value = "/new_video_list")
+    @ResponseBody
+    JSONResult newVideoList(HttpServletRequest request, HttpServletResponse response) {
+        return null;
+    }
+
+    @RequestMapping(value = "/examine_video")
+    @ResponseBody
+    JSONResult examineVideo(HttpServletRequest request, HttpServletResponse response) {
+        return null;
+    }
+
+
+}
