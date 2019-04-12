65db07ca75eaa2404d069945d570af8daca8617a
chenzhao <chenzhao5001@163.com>
2019-04-12 23:50:36 +0800

    调整

--- src/main/java/com/guidesound/controller/OrderController.java
+++ src/main/java/com/guidesound/controller/OrderController.java
@@ -434,6 +434,12 @@ public class OrderController extends BaseController {
                 int class_number = 1000000000 + classRoom.getClass_id();
                 iOrder.addRoomNumber(classRoom.getClass_id(), class_number);
                 course.setId(classRoom.getClass_id());
+                if(orderInfo.getType() == 0) {
+                    course.setAll_hours(orderInfo.getAll_hours());
+                    course.setPrice_one_hour(orderInfo.getPrice_one_hour());
+                    course.setAll_charge(orderInfo.getAll_charge());
+
+                }
                 //创建补充课堂信息
                 iOrder.ClassRoomCourse(course);
                 if (group_id != null) {
